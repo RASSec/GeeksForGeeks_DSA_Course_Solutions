62c50e87f18ccafb13d9e6bada0823ddc106c761
thecoducer <mayukh5741@gmail.com>
2019-07-18 22:07:30 +0530

    update

--- Mathematics/More/modular_division.cpp
+++ Mathematics/More/modular_division.cpp
@@ -1,15 +1,33 @@
-// C++ program to do modular division 
-#include<iostream> 
+#include<bits/stdc++.h> 
 using namespace std; 
 
-// C function for extended Euclidean Algorithm 
-int gcdExtended(int a, int b, int *x, int *y); 
+int extendedGCD(int a, int b, int *x, int *y); 
 
 // Function to find modulo inverse of b. It returns 
 // -1 when inverse doesn't 
+int main(void){
+	int t, a, b, m;
+	scanf("%d", &t);
+	while(t--){
+		scanf("%d %d %d", &a, &b, &m);
+		modDivide(a, b, m);
+	}
+}
+// Function to compute a / b under m
+void modDivide(int a, int b, int m) 
+{ 
+	// Formula: (a / b) % m = ((a % m) * (b ^ -1 % m)) % m
+	a = a % m; 
+	int inv = modInverse(b, m); 
+	if (inv == -1) 
+	cout << "Division not defined"; 
+	else
+	cout << "Result of division is " << (inv * a) % m; 
+} 
+
 int modInverse(int b, int m) 
 { 
-	int x, y; // used in extended GCD algorithm 
+	int x, y; // used in extended GCD algorithm
 	int g = gcdExtended(b, m, &x, &y); 
 
 	// Return -1 if b and m are not co-prime 
@@ -51,12 +69,4 @@ int gcdExtended(int a, int b, int *x, int *y)
 	*y = x1; 
 
 	return gcd; 
-} 
-
-// Driver Program 
-int main() 
-{ 
-	int a = 8, b = 3, m = 5; 
-	modDivide(a, b, m); 
-	return 0; 
-} 
+}
