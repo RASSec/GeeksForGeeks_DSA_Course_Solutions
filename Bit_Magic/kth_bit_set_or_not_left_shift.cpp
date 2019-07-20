d9dec540ef96a7d4d07f35d529a1b053ff9441a0
thecoducer <mayukh5741@gmail.com>
2019-07-20 11:10:30 +0530

    update

---
+++ Bit_Magic/kth_bit_set_or_not_left_shift.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkKthBit(int n, int k);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long n;
+	    cin>>n;
+	    int k;
+	    cin>>k;
+	    
+	    if(checkKthBit(n, k))
+	        cout << "Yes" << endl;
+	    else
+	        cout << "No" << endl;
+	}
+	return 0;
+}
+
+bool checkKthBit(int n, int k){
+    if(n & (1 << k))
+        return true;
+    else
+        return false;
+}
