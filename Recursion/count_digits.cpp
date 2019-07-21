fb5a61bafdc751ed1288ec25f50843ac66a50036
thecoducer <mayukh5741@gmail.com>
2019-07-21 21:05:08 +0530

    update

---
+++ Recursion/count_digits.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int n)
+{
+    if(n == 0)
+        return 0;
+    else
+        return (1 + countDigits(n / 10));
+}
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    cout<<countDigits(n)<<endl;
+	    
+	    
+	}
+	return 0;
+}
---
+++ Recursion/sum_of_digits.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n)
+{
+    if(n == 0)
+        return 0;
+    else{
+        return (n % 10 + sumOfDigits(n / 10));
+    }
+}
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    cout<<sumOfDigits(n)<<endl;
+	    
+	    
+	}
+	return 0;
+}
