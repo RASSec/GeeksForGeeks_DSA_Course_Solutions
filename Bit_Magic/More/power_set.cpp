ecf79282c26d6c6982b09deeae69bc06802c113b
thecoducer <mayukh5741@gmail.com>
2019-07-21 13:40:58 +0530

    update

---
+++ Bit_Magic/More/power_set.cpp
@@ -0,0 +1,39 @@
+/** using bitwise operator we can't produce
+ * a lexicographically sorted result as
+ * required by the problem statement.
+ * To do that we have to use other
+ * techniques to solve the problem
+ */
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+void printPowerSet(char* set, int n);
+
+int main(void)
+{
+	int t, n;
+	char set[MAX];
+	cin >> t;
+	while(t--){
+	    cin >> n;
+	    for(int i=0;i<n;i++){
+            cin >> set[i];
+	    }
+	    printPowerSet(set, n);
+	}
+	return 0;
+}
+
+void printPowerSet(char* set, int n){
+    unsigned int subsetNos = pow(2, n);
+    
+    for(int i=0;i<subsetNos;i++){
+        for(int j=0;j<n;j++){
+            if(i & (1 << j)){
+                cout << set[j];
+            }
+        }
+        cout << " ";
+    }
+}
