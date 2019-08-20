89c9076a31f5260f91a2c613a82bab4182167b2b
thecoducer <mayukh5741@gmail.com>
2019-08-20 13:50:36 +0530

    update

--- Recursion/count_digits.cpp
+++ Recursion/count_digits.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 using namespace std;
 
+// My way
 int countDigits(int n)
+{
+    if(n < 10)
+        return 1;
+    else
+        return n/n + countDigits(n / 10);
+    
+}
+
+/* int countDigits(int n)
 {
     if(n == 0)
         return 0;
     else
         return (1 + countDigits(n / 10));
-}
+} */
 
 int main()
 {
