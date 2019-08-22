6894d45c87bfabb6730352dd03b27cdd3d29035f
thecoducer <mayukh5741@gmail.com>
2019-08-22 12:30:05 +0530

    update

--- Recursion/power_using_recursion.cpp
+++ Recursion/power_using_recursion.cpp
@@ -22,3 +22,23 @@ int RecursivePower(int n,int p)
     else
         return n * RecursivePower(n, p-1);
 }
+
+/** Another way that takes O(log n):
+int RecursivePower(int n,int p)
+{
+	int t = 1;
+
+    if(p == 1){
+		return n;
+	}
+
+	t = RecursivePower(n, p/2);
+	if((p%2) == 0){
+		return (t * t);
+	}else
+	{
+		return (t * t * n);
+	}
+	
+}
+**/
